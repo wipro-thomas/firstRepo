8ae4be1ccb9c8ef90375342c415723f7ca83f9bc
wipro-thomas <32334420+wipro-thomas@users.noreply.github.com>
2017-09-27 16:59:11 +0100

    Add files via upload

---
+++ JF_1.java
@@ -0,0 +1,16 @@
+//Write a program to accept 2 strings from the user (via command line).
+//Your program must then concatenate them and print the resultant string.  
+
+public class JF_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		
+
+	}
+
+}
+
+
